bcdc9669c775d7bd455210b7be1fb5347bc53805
tcb <tcb@tencent.com>
2024-01-21 06:17:08 +0000

    init

--- wxcloudrun/models.py
+++ wxcloudrun/models.py
@@ -3,13 +3,15 @@ from datetime import datetime
 from django.db import models
 
 
-# Roll a dice model
-class Rolls(models.Model):
+# Create your models here.
+class Counters(models.Model):
     id = models.AutoField
-    discount = models.IntegerField(max_length=11, default=10)
+    count = models.IntegerField(max_length=11, default=0)
     createdAt = models.DateTimeField(default=datetime.now(), )
+    updatedAt = models.DateTimeField(default=datetime.now(),)
+
     def __str__(self):
         return self.title
 
     class Meta:
-        db_table = 'Rolls'  # 数据库表名
+        db_table = 'Counters'  # 数据库表名
